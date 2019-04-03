ff0a7fd96bb96f95d54eab51e94e9b3685b96dac
Alex Rawson <ajzecrom@gmail.com>
2019-04-03 07:04:25 -0500

    Now there's a window, next up is tiles

---
+++ gui.py
@@ -0,0 +1,39 @@
+import tetris_core
+import os, pygame
+
+class Tetris_Board_Renderer:
+    def __init__(self, board, size, background_image, tiles):
+        self.board = board
+        self.size = size
+        self.background_image = background_image
+        self.tiles = tiles
+
+    def render(self):
+        surface = pygame.Surface(self.size)
+        surface.fill((255, 255, 255))
+        surface.blit(self.background_image, (0, 0))
+
+        return surface
+
+tiles = []
+for file in os.listdir("Assets/Tiles/"):
+    tiles.append(pygame.image.load("Assets/Tiles/" + file))
+background_image = pygame.image.load("Assets/grid.png")
+
+def main():
+    game = tetris_core.Tetris_Game(7, (12, 20), 1)
+    board = Tetris_Board_Renderer(game.boards[0], (12 * 32, 20 * 32), background_image, tiles)
+    running = True
+    pygame.init()
+    screen = pygame.display.set_mode((12 * 32, 20 * 32))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        display = board.render()
+        screen.blit(display, (0,0))
+        pygame.display.flip()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
